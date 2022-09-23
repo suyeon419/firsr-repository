e4e225f317ab6a0be9fe8645ffbbd91da54b327a
suyeon419 <113648382+suyeon419@users.noreply.github.com>
2022-09-23 10:07:27 +0900

    sw

--- prime_number.py
+++ prime_number.py
@@ -4,4 +4,6 @@ def is_prime_number(x):
             print("The number {num1} is not a prime number.".format(num1=x))
             return
     print("The number {num1} is a prime number.".format(num1=x))
+    룰루랄라
+    
 
